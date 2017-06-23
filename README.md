0d976007ce17d9ccd1047005bdcc01d80bbd3b10
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-06-23 18:42:57 +0200

    Structure project with back and front layers, added some styles to frontend layer

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Basic Node Chat
+# Very basic and minimalistic Recipe app
+
+The idea is to build a **Recipe app** using node in the backend and javascript (no framework) in the frontend.
---
+++ server-layer/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+// set the view engine to ejs
+app.set('views', 'app/views');
+app.set('view engine', 'ejs');
+
+const port = process.env.PORT || 9000;
+
+app.get('/', function(req, res) {
+    res.render('home', {title: 'The Simplest Recipe App'});
+});
+
+app.get('/user', function(req, res) {
+    res.send('User profile view');
+});
+
+app.listen(port, function() {
+   console.log(`App running in http://localhost:${port}`);
+});
